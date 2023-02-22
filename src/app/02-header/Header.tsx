ea64b9a078ebdceb509289fd22a186d6d8b23d75
Jaison Francisco <jaison2@yahoo.com>
2023-02-21 23:22:00 -0500

    update

--- src/app/02-header/Header.tsx
+++ src/app/02-header/Header.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export const Header = () => {
     return <div>
         This is Header
--- src/app/04-content/Content.tsx
+++ src/app/04-content/Content.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './Content.scss';
 
 export const Content = () => {
