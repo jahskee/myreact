3fc6275aed8465b257258f57ead35aac83ee4819
Jaison Francisco <jaison2@yahoo.com>
2023-02-21 23:47:57 -0500

    update

--- src/app/02-header/Header.tsx
+++ src/app/02-header/Header.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const Header = () => {
+    
+    useEffect(()=>{
+        setInterval(() => setCurrentDateTime(new Date()), 1000);
+        return ()=>{
+            console.log('Cleanup function when component will unmount');
+        }
+    })
+
+    const [currentDateTime, setCurrentDateTime] = useState(new Date())
+    
     return <div>
-        This is Header
+        This is Header {currentDateTime.toString()}
     </div>;
 }
