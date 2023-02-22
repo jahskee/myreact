990647221d3226c0b940ced70521f275406d9d16
Jaison Francisco <jaison2@yahoo.com>
2023-02-21 23:20:57 -0500

    update

--- src/app/01-home/MainApp.tsx
+++ src/app/01-home/MainApp.tsx
@@ -3,7 +3,6 @@ import { Content } from '../04-content/Content';
 import { Header } from '../02-header/Header';
 import './MainApp.scss';
 
-
 export const MainApp = () => 
     <div className="container">
         <Header />
